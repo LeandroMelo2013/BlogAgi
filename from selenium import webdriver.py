2a455b5a6cf059b1f59eead9501c931fbfbf45bf
LeandroMelo2013 <146021695+LeandroMelo2013@users.noreply.github.com>
2024-01-17 15:30:48 -0300

    Cenarios de Teste

--- from selenium import webdriver.py
+++ from selenium import webdriver.py
@@ -10,14 +10,20 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 url = "https://blogdoagi.com.br/"
 driver.get(url)
 
+# Cenario 1
 # Encontrar o campo de pesquisa e enviar a consulta
-search_open = driver.find_element("Pesquisar")
+search_open = id="search-open"("Pesquisar")
 search_query = "Sua consulta de pesquisa"
 search_open.send_keys(search_query)
 search_open.send_keys(Keys.RETURN)
 
 # Aguardar um pouco para a página carregar os resultados
 time.sleep(3)
+
+# Cenario 2
+# Verificar se a página contém as informações esperadas
+assert "Resultado da Pesquisa" in driver.title
+assert "Informação 1" in driver.page_source
 
 # Realizar ações adicionais, se necessário
 # ...
